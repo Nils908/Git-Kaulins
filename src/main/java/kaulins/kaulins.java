3e13a4cf21249ae09d5b72c52bca3817054bdf5a
Nils908 <nils18733@gmail.com>
2026-09-07 09:55:59 +0300

    v2

--- src/main/java/kaulins/kaulins.java
+++ src/main/java/kaulins/kaulins.java
@@ -3,10 +3,14 @@ package kaulins;
 import java.util.Random;
 
 public class kaulins {
-    public class void main(String[] args) {
+    static void mestKaulinu(){
         int skaitlis;
         Random rand = new Random();
-        skaitlis = rand.nextInt(6)+1;
-        System.out.println("Uzmestais skaitlis"+skaitlis);
+        skaitlis = rand.nextInt(6) + 1;
+        System.out.println("Uzmestais skaitlis " + skaitlis);
+    }
+
+    public static void main(String[] args) {
+        mestKaulinu();
     }
 }
