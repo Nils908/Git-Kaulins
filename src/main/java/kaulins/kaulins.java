8a1506100c31691e4450e87d529ef779f60d1b55
Nils908 <nils18733@gmail.com>
2026-09-16 14:05:51 +0300

    izvada mazako skaitli

--- src/main/java/kaulins/kaulins.java
+++ src/main/java/kaulins/kaulins.java
@@ -4,14 +4,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class kaulins {
-    static void mestKaulinu(int reizes){
-        int skaitlis;
+    static int mestKaulinu(int reizes){
+        int skaitlis, min = 6;
         Random rand = new Random();
-        for(int i
-            = 1; i <= reizes; i++){
+        for(int i = 1; i <= reizes; i++) {
             skaitlis = rand.nextInt(6) + 1;
             System.out.println("Uzmests skaitlis " + skaitlis);
+            if(skaitlis < min){
+                min = skaitlis;
+            }
         }
+        return min;
     }
 
     public static void main(String[] args) {
@@ -19,7 +22,7 @@ public class kaulins {
         Scanner scan = new Scanner(System.in);
         System.out.print("Cik reizes metisi: ");
         reizes = scan.nextInt();
-        mestKaulinu(reizes);
+        System.out.print("Mazkais mestais skaitlis:  " + mestKaulinu(reizes));
         scan.close();
     }
 }
